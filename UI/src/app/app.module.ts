4e2a9a57b704b07f069686c16c7408c68632f877
DESKTOP-3E4KQ2J\Ahmad <Ahmad-Bunni@hotmail.com>
2022-04-30 15:45:32 +0200

    lazy loading

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app.routing'
-import { LoginModule } from './modules/login/login.module'
 import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, LoginModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
